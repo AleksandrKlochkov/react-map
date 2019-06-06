4c609140767c5332de596a426a139e89b8236eee
Alexandr Klochkov <surfer.ru@mail.ru>
2019-06-06 15:12:27 +0400

    add map

--- src/components/hoc/Layout/Layout.js
+++ src/components/hoc/Layout/Layout.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react'
 import { directive } from '@babel/types';
+import classes from './Layout.css'
+import GogleMap from '../../maps/GoogleMap/GogleMap'
 
 class Layout extends Component{
     render(){
         return(
-            <div>
-                <h1>Layout</h1>
+            <div className={classes.Layout}>
+               <ul>
+                   <li></li>
+                   <li><GogleMap/></li>
+               </ul>
             </div>
         )
     }
