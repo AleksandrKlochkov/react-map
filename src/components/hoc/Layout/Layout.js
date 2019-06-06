70033a09d7dd09cf9c90d4d8c53ede6c2fc26d97
Alexandr Klochkov <surfer.ru@mail.ru>
2019-06-06 14:07:23 +0400

    add Loyout

---
+++ src/components/hoc/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import { directive } from '@babel/types';
+
+class Layout extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Layout</h1>
+            </div>
+        )
+    }
+
+}
+
+
+export default Layout 
